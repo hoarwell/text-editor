fff8e3b26a642da006c4b76ae140a890a344b679
YoungJo Jeong <youngjo@YoungJos-MacBook-Air.local>
2021-06-09 13:56:54 +0900

    21.06.09. 13:55 이미지 넣을 수 있게 input추가, 기존의 contenteditable=true div를 통한 텍스트 에디터 추가 그리고 약간의 css 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const contentRef = useRef();
+
+  const fileChange = (e) => {
+    const { files } = e.target;
+    const file = files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = (finishedEvent) => {
+      const { currentTarget : { result } } = finishedEvent;
+      console.log(result);
+      insertImage(result)
+    }
+  }
+
+  const insertImage = (result) => {
+      contentRef.current.focus();
+      document.execCommand('insertHTML', false, `<img className = "content-image" src = ${result} />`);
+  }
+
+  const contentChange = (e) =>{
+    const { value, innerHTML } = e.target;
+    console.log(innerHTML);
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if( name === "title" ){
+      console.log(value);
+    } 
+  }
+
+  useEffect(() => {
+    contentRef.current.addEventListener('input', contentChange);
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "container">
+        <p>wysiwyg text editor</p>
+        <input className = "title" name = "title" type = "text" placeholder = "title" onChange = { handleChange } require/>
+        <input className = "file" name = "file" type = "file" accept = "image/*" onChange = { fileChange }/>
+        <div className = "content" name = "content" contentEditable="true" ref = { contentRef }></div>
+      </div>
     </div>
   );
 }
